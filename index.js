6368d09d212bbfbd7c665587f31a47972983f2ef
amparo1206 <amparo.frontend@gmail.com>
2022-01-19 00:24:54 +0100

    añadiendo un POST que faltaba

--- index.js
+++ index.js
@@ -179,7 +179,17 @@ app.put('/updateProducto/:id',(req,res)=>{
        res.send("Producto actualizado...");
     })
 })
-  
+
+app.post('/', (req,res)=>{
+    let post = {title: req.body.title, body: req.body.body};
+    let sql = 'INSERT INTO posts SET ?'
+    db.query(sql,post,(err,result)=>{
+      if(err) throw err;
+      console.log(result);
+      res.send('Post added...');
+    })
+})
+
   
 
 
